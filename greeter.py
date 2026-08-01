db3afc17325ef2ea323617ef67578abe96b555f4
claudiosousasantos <admclaudiosantos@gmail.com>
2026-08-01 06:26:53 -0300

    Add multilingual greeting function
    Prints a greeting in Spanish, French, or English based on a language code input.

---
+++ greeter.py
@@ -0,0 +1,10 @@
+def greet (lang):
+    if lang == 'es':
+        print("Hola")
+    elif lang == 'fr':
+        print("Bonjour")
+    else:
+        print('Hello')
+greet('es')
+greet('fr')
+greet('en')
